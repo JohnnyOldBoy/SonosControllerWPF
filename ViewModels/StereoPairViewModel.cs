e294ed4365ed2f4a59b5366fe25ad783c79b3ad6
Greg Chelstowski <dustcloud@hotmail.co.uk>
2021-03-10 23:05:22 +0100

    Styling of the MusicLibraryWindow done.

--- ViewModels/StereoPairViewModel.cs
+++ ViewModels/StereoPairViewModel.cs
@@ -1,24 +1,35 @@
 ﻿using Devices;
 using Services;
 using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight;
 
 namespace SonosController.ViewModels
 {
-    public class StereoPairViewModel
+    //Make sure your VIEWMODELS derive from ViewModelBase
+    //Do the same with zonegroupviewmodel
+    public class StereoPairViewModel : ViewModelBase
     {
         private string _pairName = string.Empty;
 
         public string PairName 
         { 
-            get => _pairName; 
-            set => _pairName = value; 
+            get => _pairName;
+            set
+            {
+                _pairName = value; 
+                RaisePropertyChanged(nameof(PairName));
+            }
         }
 
         private ObservableCollection<StereoPair> _stereoPair = new ObservableCollection<StereoPair>();
         public ObservableCollection<StereoPair> StereoPair
         { 
-            get => _stereoPair; 
-            set => _stereoPair = value; 
+            get => _stereoPair;
+            set
+            {
+                _stereoPair = value; 
+                RaisePropertyChanged(nameof(StereoPair));
+            }
         }
     }
 }
